95581486bec3fc23a7b9e7164aa30adadbf84362
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2019-05-20 22:15:22 -0300

    Update main.sh

--- src/os/install/macos/main.sh
+++ src/os/install/macos/main.sh
@@ -7,18 +7,18 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
-./homebrew.sh
-./bash.sh
-./../nvm.sh
-./compression_tools.sh
+#./homebrew.sh
+#./bash.sh
+#./../nvm.sh
+#./compression_tools.sh
 ./git.sh
 ./gpg.sh
-./image_tools.sh
+# ./image_tools.sh
 ./misc.sh
 ./misc_tools.sh
 ./../npm.sh
 ./tmux.sh
-./video_tools.sh
+# ./video_tools.sh
 ./../vim.sh
 ./web_font_tools.sh
 
