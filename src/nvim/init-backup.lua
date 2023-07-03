53197372bd1daf10300454f9e9de86b9499f0ee9
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-07-03 22:02:42 +0200

    add colorizer files and modify init-backup.lua

--- src/nvim/init-backup.lua
+++ src/nvim/init-backup.lua
@@ -1,4 +1,19 @@
 local vimrc = vim.fn.stdpath("config") .. "/init-backup.vim"
 vim.cmd.source(vimrc)
 
-require('nvim-picgo').setup()
+----------------------------
+--  nvim colorizer setup  --
+----------------------------
+
+require("nvim-picgo").setup()
+local auto_dark_mode = require('auto-dark-mode')
+auto_dark_mode.setup({
+  update_interval = 1000,
+  set_dark_mode = function()
+    vim.cmd("call ChangeTheme('light')")
+  end,
+  set_light_mode = function()
+    vim.cmd("call ChangeTheme('dark')")
+  end,
+})
+auto_dark_mode.init()
